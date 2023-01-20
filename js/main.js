d5f1804a78e2947691df8e42258c68a8e4a4a657
bahodir <bahodirataboyev7@gmail.com>
2023-01-20 11:46:22 +0500

    find

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let elForm = document.querySelector('.form')
+let elInp = document.querySelector('.inp')
+let elBtn = document.querySelector('.btn')
+
+
+const price = [100,200,300,400,500,600,700];
+
+
+elForm.addEventListener('submit', (e)=>{
+    price.forEach(i){
+        
+    }
+})
